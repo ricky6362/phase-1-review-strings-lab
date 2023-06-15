9ac123bbcac53efa674ea73e6a8c0ba6453ca28a
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-15 16:41:38 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+// creating a variable with the name currentUser
+const currentUser = "John Wekesa"
+
+// Welcome message
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+// excited welcome message
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase()}!`
+
+// short greeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
+console.log(excitedWelcomeMessage);
